a09c28da5f2639944ee0e66af9ed31f6fbcdd959
Midas1618 <nhatanh.wordofmouth@gmail.com>
2022-02-21 08:24:53 +0000

    Nhật Anh pust file nháp player 3

--- code_action/player3.py
+++ code_action/player3.py
@@ -17,8 +17,43 @@
 '''
 from init_game import convert
 
-def action(player, card_normal, card_point, conis):
+def phanloai(card):
+    if card["upgrade"] >0:
+        return "finisher",card["upgrade"]*10
+    if sum(card["give_back"].values()) == 1:
+        score = 0
+        for idnl in range(4):
+            score += (1+idnl)*(list(card["receive"].values())[idnl]-list(card["give_back"].values())[idnl])
+        return "crawler",score
+    if sum(card["give_back"].values()) == 0:
+        score = 0
+        for idnl in range(4):
+            score += (1+idnl)*(list(card["receive"].values())[idnl])
+        return "birth",score*5
+    if sum(card['give_back'].values()) == sum(card['receive'].values()):
+        for idnl in range(4):
+            if list(card['give_back'].values())[idnl] > 0:
+                score = 4-idnl
+                return "trader",score*2
+    return None, 0
 
+def action(player, card_normal, card_point, conis):
+    diem = 0
+    card_n = None
+    for card in card_normal:
+        cost = card_normal.index(card)
+        if cost <= player.material["yellow"]:
+            ctype,score = phanloai(card)
+            if score > diem:
+                diem = score
+                card_n = card
+    #nếu có thẻ normal đẹp thì lấy
+    if card_n != None:
+        fee = str(card_normal.index(card_n)) + "-0-0-0"
+        print(fee,card_normal.index(card))
+        return 'get_card_normal', card_n, convert(fee)
+    # else:
+        #nếu không thì chạy engine
     return 'relax'
 
                  
